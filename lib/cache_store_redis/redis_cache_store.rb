4a096ea594311f20b4a146d1da97b348692f0d38
Cosima Radu <cosima.radu@sage.com>
2025-03-31 22:14:39 +0100

    ENG-2688 - Resolve Redis warnings

--- lib/cache_store_redis/redis_cache_store.rb
+++ lib/cache_store_redis/redis_cache_store.rb
@@ -75,10 +75,9 @@ class RedisCacheStore
     expire_value = expiry_int.positive? ? expiry_int : Integer(DEFAULT_TTL)
 
     with_client do |client|
-      client.multi do
-        client.set(k, v)
-
-        client.expire(k, expire_value)
+      client.multi do |pipeline|
+        pipeline.set(k, v)
+        pipeline.expire(k, expire_value)
       end
     end
   end
